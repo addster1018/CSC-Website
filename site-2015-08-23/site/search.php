46d61dd08d993be49d82767381fff7409e37440e
addster1018 <addster1018@gmail.com>
2015-08-30 13:42:57 -0400

    The code
    EVRRYTHING

---
+++ site-2015-08-23/site/search.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <title>Search</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Links -->
+
+    <link rel="stylesheet" href="css/search.css">
+
+
+    <!--JS-->
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="js/rd-smoothscroll.min.js"></script> 
+
+    <!--[if lt IE 9]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+            <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.js"></script>
+    <![endif]-->
+    <script src='js/device.min.js'></script>
+  </head>
+  <body>
+  <div class="page">
+  <!--========================================================
+                            HEADER
+  =========================================================-->
+    
+    <header class="head1">
+            <div class="top-panel wow fadeInRight" data-wow-duration="1.5s">
+                <div class="container">
+                    <a href="callto:#" class="phone-icon">+1 800 559 6580</a>
+                    <address>28 Jackson Blvd Ste 1020 Chicago</address>
+                </div>
+            </div>
+            <div id="stuck_container" class="stuck_container">
+                <nav class="navbar navbar-default navbar-static-top">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <h1 class="navbar-brand">
+                                <a data-type='rd-navbar-brand' href="./">Icer</a>
+                            </h1>
+                        </div>
+
+                        <ul class="navbar-nav sf-menu navbar-right" data-type="navbar">
+                            <li>
+                                <a href="./">Home</a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="index-1.html">About</a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="#">Lorem ipsum dolor </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Ait amet conse </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Ctetur adipisicing</a>
+                                        <ul class="dropdown-menu">
+                                            <li>
+                                                <a href="#">Latest</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Archive</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#">Sed do eiusmod </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Tempor incididunt </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="index-2.html">Services</a>
+                            </li>
+                            <li>
+                                <a href="index-3.html">Gallery</a>
+                            </li>
+
+                            <li>
+                                <a href="index-4.html">Contacts</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </nav>
+            </div>
+
+        </header>
+
+  <!--========================================================
+                            CONTENT
+  =========================================================-->
+  <main class="bg1">
+    <section id="content" class="content well">
+        <div class="container">
+          <form class="search-form" action="search.php" method="GET" accept-charset="utf-8">
+            <label class="search-form_label">
+              <input class="search-form_input" type="text" name="s" autocomplete="off" placeholder=""/>
+              <span class="search-form_liveout"></span>
+            </label>
+            <button class="search-form_submit btn btn-primary" type="submit">Search</button>
+          </form>
+            <h3>Search Results</h3>
+            <div id="search-results"></div>
+        </div>
+    </section>
+  </main>  
+
+  <!--========================================================
+                            FOOTER
+  =========================================================-->
+  <footer>
+          <section class="bot_sec bot_sec_mod center767">
+              <div class="container">
+                  <p class="rights">
+                      Icer &#169; <span id="copyright-year"></span> &nbsp;|&nbsp;
+                      <a href="index-5.html"> Privacy Policy</a>
+                  </p>
+
+                  <ul class="inline-list">
+                      <li><a href="#" class="fa fa-facebook-square">facebook</a></li>
+                      <li><a href="#" class="fa  fa-google-plus-square">google-plus</a></li>
+                      <li><a href="#" class="fa fa-twitter">twitter</a></li>
+                      <li><a href="#" class="fa fa-pinterest">pinterest</a></li>
+                      <li><a href="#" class="fa fa-linkedin-square">linkedin</a></li>
+                      <li><a href="#" class="fa  fa-instagram">instagram</a></li>
+                  </ul>
+              </div>
+          </section>
+      </footer>
+  </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- Include all compiled plugins (below), or include individual files as needed -->         
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/tm-scripts.js"></script>    
+  <!-- </script> -->
+
+
+  </body>
+</html>
